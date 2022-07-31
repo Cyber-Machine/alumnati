de003392fae793fc757fb4450b0ec565a47a396d
Maaz Karim <maazkarim02@gmail.com>
2022-07-31 19:57:50 +0530

    Updated AlumniList

--- src/Components/AlumniList.js
+++ src/Components/AlumniList.js
@@ -25,7 +25,7 @@ export default function AlumniList() {
 
         <div className="bg-white">  
           <div className="container px-6 py-10 mx-auto">
-                <h1 className="text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl dark:text-white">Our Alumni</h1>
+                <h1 className="text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl ">Our Alumni</h1>
                 
                 {/* <p className="max-w-2xl mx-auto my-6 text-center text-gray-500 dark:text-gray-300">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt ex placeat modi magni quia error alias, adipisci rem similique, at omnis eligendi optio eos harum.
