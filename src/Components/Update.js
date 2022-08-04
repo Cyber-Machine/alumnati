12ab6a24f2a631aeac0d996600f9fa23356d82aa
Maaz Karim <maazkarim02@gmail.com>
2022-08-05 01:22:32 +0530

    Updated Update.js

--- src/Components/Update.js
+++ src/Components/Update.js
@@ -1,7 +1,6 @@
-import { useState,useEffect } from "react";
+import { useState , useEffect , useContext} from "react";
 import {db} from "../Firebase";
-import {collection ,query, getDocs,where,limit,addDoc, updateDoc, doc, orderBy} from "firebase/firestore";
-import { useContext } from 'react';
+import {collection ,query, getDocs,where, updateDoc, doc} from "firebase/firestore";
 import { AuthContext } from "../Context/AuthContext";
 export const Update = () => {
     const {currentUser} = useContext(AuthContext);
