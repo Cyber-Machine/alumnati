dde315ff36207ff662bd31c950104154c7c68e3d
Maaz Karim <maazkarim02@gmail.com>
2022-08-16 00:12:22 +0530

    Added CalendarModule.js

--- src/Pages/AlumniPage.js
+++ src/Pages/AlumniPage.js
@@ -1,5 +1,4 @@
 import { Nav } from "../Components/Nav"
-import Index from "../Components/Team"
 import AlumniList from "../Components/AlumniList"
 import { Footer } from "../Components/Footer"
 export default function AlumniPage(){
