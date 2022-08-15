100f7a38c4338520a067d9109c5714bd9ac33e47
Maaz Karim <maazkarim02@gmail.com>
2022-08-16 00:10:02 +0530

    Linked SignUp

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -78,7 +78,7 @@ export default function LoginPage(){
 
                         </form>
 
-                        <p className="mt-6 text-sm text-center text-gray-400">Don&#x27;t have an account yet? <a href="#" className="text-blue-500 focus:outline-none focus:underline hover:underline">Sign up</a>.</p>
+                        <p className="mt-6 text-sm text-center text-gray-400">Don&#x27;t have an account yet? <a href="/SignUp" className="text-blue-500 focus:outline-none focus:underline hover:underline">Sign up</a>.</p>
                     </div>
                 </div>
             </div>
