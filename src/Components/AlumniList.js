55254f31b357eb47774a80dabf6d85cc6b6a9227
Maaz Karim <maazkarim02@gmail.com>
2022-08-31 13:13:17 +0530

    Added PortfolioPage

--- src/Components/AlumniList.js
+++ src/Components/AlumniList.js
@@ -81,7 +81,7 @@ export default function AlumniList() {
             ( <div className="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center">
                     <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
             </div> ) :
-            (show.map((a)=><AlumniCard Name={a.Name} Designation={a.Department} Company={a.Domain} Github={a.Github} Linkedin={a.LinkedIn} Twitter={a.Twitter} ImageURL={a.imageURL}/>))
+            (show.map((a)=><AlumniCard Name={a.Name} Designation={a.Department} Company={a.Domain} Github={a.Github} Linkedin={a.LinkedIn} Twitter={a.Twitter} ImageURL={a.imageURL} id={a.uid}/>))
           }
         </div>
         </div>
---
+++ src/Pages/Portfolio.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react'
+import {useLocation ,Navigate} from 'react-router-dom'
+import {collection ,query, getDocs,where, updateDoc, doc} from "firebase/firestore";
+import { db } from '../Firebase';
+import { Nav } from '../Components/Nav'
+import { Footer } from '../Components/Footer'
+import About from '../Components/Portfolio/About'
+import Home from '../Components/Portfolio/Home'
+import Work from '../Components/Portfolio/Work'
+import Blogs from '../Components/Portfolio/Blogs'
+function Portfolio() {
+    const location = useLocation();
+    const { id } = location.state || "";
+    console.log(id);
+    const RequireID = ({children}) => {
+        return id ? (children) : <Navigate to = "/Alumni"/>
+      }
+    useEffect(() => {
+      window.scrollTo(0 , document.body.scrollHeight);
+      window.scrollTo({top:0 , behavior:"smooth"}); 
+    }, [])
+    
+    const [res, setRes] = useState([]);
+    const [Name,setName]=useState("")
+    const [dept,setDept]=useState("");
+    const [domain,setDomain]=useState("");
+    const [github, setgithub] = useState("");
+    const [linkedin, setLinkedin] = useState("");
+    const [Twitter, setTwitter] = useState("");
+    const [projects, setProjects] = useState([]);
+    const [blogs, setBlogs] = useState([]);
+    useEffect(()=>{
+    const getUsers=async()=>{
+        const data=await getDocs(query(collection(db,"Alumni"), where("uid","==",id)));
+        setRes(data.docs.map((doc)=>({...doc.data(),id:doc.id})));
+    }
+    getUsers();
+    
+    },[]);
+
+    const getBlogs = async() => {
+        const list = await getDocs(query(collection(db , "Blog"),where("uid","==",id)));
+        setBlogs(list.docs.map( (doc) => ({id:doc.id , ...doc.data()}) ));
+      }
+    const getProjects = async() =>{
+        const list = await getDocs(query(collection(db,"Projects"),where("uid","==",id)));
+        setProjects(list.docs.map((doc) => ({id:doc.id , ...doc.data()})));
+      }
+      useEffect(() => {
+          getBlogs();
+          getProjects();
+          console.log({blogs})
+      },[])
+
+    useEffect(() => {
+        if(res.length >0)
+        {
+            setName(res[0].Name);
+            setTwitter(res[0].Twitter);
+            setLinkedin(res[0].LinkedIn);
+            setgithub(res[0].Github);
+            setDomain(res[0].Domain);
+        }
+    
+    }, [res])
+  
+    
+    return (
+    <>
+        <Nav />
+        <RequireID>
+        <Home Name={Name} Linkedin={linkedin} Github={github} Designation={domain} Twitter={Twitter}/>
+        <About Name={Name} Bio={"I am "+domain+" Enthusiast."}/>
+        <Work data={projects}/>
+        <Blogs data={blogs}/>
+        </RequireID>
+        <Footer />
+    </>
+  )
+}
+
+export default Portfolio
